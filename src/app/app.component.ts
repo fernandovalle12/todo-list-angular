8f8233da7c6a661d754414e6a497f828f21cc3c8
Fernando Valle <fernando.jvalle@hotmail.com>
2021-10-19 20:19:30 -0300

    Todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import * as uuid from 'uuid';
+
+interface todoArr {
+    id: string,
+    value: string
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-list';
+
+export class AppComponent implements OnInit {
+    public todoList: todoArr[] = [];
+    public formGroup: FormGroup = new FormGroup({});
+
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private toastr: ToastrService
+    ) { }
+
+    ngOnInit() {
+        this.formGroup = this.getFormGroup();
+    }
+
+    private getFormGroup() {
+        return this.formBuilder.group({
+            todoInput: [
+                { value: null, disabled: false },
+                Validators.compose([Validators.required])
+            ]
+        })
+    }
+
+    public onAdd() {
+        if(!this.formGroup.valid) {
+            this.toastr.error('Por favor digite algo no input', 'Input vazio!');
+            return;
+        }
+        this.todoList.push({ id: uuid.v4(), value: this.formGroup.controls.todoInput.value });
+        this.formGroup.controls.todoInput.reset();
+    }
+
+    public onRemove(todo: todoArr) {
+        const index = this.todoList.findIndex(val => val.id == todo.id);
+        this.todoList.splice(index, 1);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
